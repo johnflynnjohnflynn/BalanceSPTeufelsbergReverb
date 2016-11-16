d6c8442cccc8c85143320e8cc39b86abb211892f
John Flynn <johnflynn@users.noreply.github.com>
2016-11-16 22:00:28 +0000

    Various refactoring.

--- include/Aidio/Buffer.h
+++ include/Aidio/Buffer.h
@@ -28,8 +28,6 @@
 #define BUFFER_H_INCLUDED
 
 #include <vector>
-#include <algorithm>
-#include <iostream>
 #include "gsl.h"
 
 namespace ado
@@ -84,40 +82,14 @@ public:
     ~Buffer() {}
 
     int numChannels() const { return bufferData.size(); }
-    int numSamples()  const { return (numChannels() > 0) ? bufferData[0].size() : 0; }
+    int numSamples()  const { return bufferData[0].size(); }
 
     float** getWriteArray() { return pointerAccess.data(); }
     const float** getReadArray() const { return const_cast<const float**> (pointerAccess.data()); } // const!? Really?!
 
-    std::vector<std::vector<float>>::iterator       begin()       { return bufferData.begin(); }
-    std::vector<std::vector<float>>::const_iterator begin() const { return bufferData.begin(); }
-    std::vector<std::vector<float>>::iterator       end()         { return bufferData.end(); }
-    std::vector<std::vector<float>>::const_iterator end()   const { return bufferData.end(); }
-
-    void clear()
-    {
-        for (auto& chan : bufferData)
-            std::fill (chan.begin(), chan.end(), 0.0f);
-    }
-
-    void fillAllOnes()
-    {
-        for (auto& chan : bufferData)
-            std::fill (chan.begin(), chan.end(), 1.0f);
-    }
-
-    void fillAscending()
-    {
-        for (auto& chan : bufferData)
-        {
-            float sum = 1.0f;
-            for (auto& samp : chan)
-            {
-                samp = sum;
-                sum += 1.0f;
-            }
-        }
-    }
+    void clear();
+    void fillAllOnes();
+    void fillAscending();
 
 private:
     std::vector<std::vector<float>> bufferData;
--- src/Utility.cpp
+++ src/Utility.cpp
@@ -24,6 +24,7 @@
 */
 //--------//--------//--------//--------//--------//--------//--------//--------
 
+#include <iostream>
 #include "Aidio/Utility.h"
 #include "Aidio/Buffer.h"
 
