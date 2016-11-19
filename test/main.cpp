48a824e159b3d655cb1764dec5625fdf1af7812a
John Flynn <johnflynn@users.noreply.github.com>
2016-11-19 16:49:38 +0000

    Replace Catch with JUCE unit testing as in Aidio.

--- test/main.cpp
+++ test/main.cpp
@@ -14,42 +14,48 @@ int main (int argc, const char* argv[])
 
 //--------//--------//--------//--------//--------//--------//--------//--------
 
-AIDIO_DECLARE_UNIT_TEST_WITH_STATIC_INSTANCE(Resample)
+AIDIO_DECLARE_UNIT_TEST_WITH_STATIC_INSTANCE(Helper)
 
-Resample::Resample() : UnitTest ("Resample") {}
+Helper::Helper() : UnitTest ("Helper") {}
 
-void Resample::runTest()
+void Helper::runTest()
 {
+    beginTest ("bufferFillAllOnes()");
 
-beginTest ("bufferFillAllOnes()"); {
-    AudioBuffer<float> buffer {2, 32};
-    jdo::bufferFillAllOnes (buffer);
-    expectEquals (buffer.getSample(1, 31), 1.0f);
-}
+    {
+        AudioBuffer<float> buffer {2, 32};
+        jdo::bufferFillAllOnes (buffer);
+        expectEquals (buffer.getSample(1, 31), 1.0f);
+    }
 
-beginTest ("bufferSumElements()"); {
-    AudioBuffer<float> buffer {1, 500};
-    jdo::bufferFillAllOnes (buffer);
-    const float sum {jdo::bufferSumElements(buffer)};
-    expectEquals (sum, 500.0f);
-}
+    beginTest ("bufferSumElements()");
 
-beginTest ("loadBufferFromWavBinaryData()"); {
-    AudioBuffer<float> buffer;
-    jdo::bufferLoadFromWavBinaryData(BinaryData::Stereo64SamplesAllOnes_wav,
-                                     BinaryData::Stereo64SamplesAllOnes_wavSize,
-                                     buffer);
-    const float sum {jdo::bufferSumElements(buffer)};
-    expectEquals (Approx(sum), 128.0f);
-}
+    {
+        AudioBuffer<float> buffer {1, 500};
+        jdo::bufferFillAllOnes (buffer);
+        const float sum {jdo::bufferSumElements(buffer)};
+        expectEquals (sum, 500.0f);
+    }
 
-beginTest ("nextPowerOf2()"); {
-    int x = 55;
-    expectEquals(jdo::nextPowerOf2(x), 64);
-    x = -234;
-    expectEquals(jdo::nextPowerOf2(x), 1);
-    x = 16300;
-    expectEquals(jdo::nextPowerOf2(x), 16384);
-}
+    beginTest ("loadBufferFromWavBinaryData()");
 
+    {
+        AudioBuffer<float> buffer;
+        jdo::bufferLoadFromWavBinaryData(BinaryData::Stereo64SamplesAllOnes_wav,
+                                         BinaryData::Stereo64SamplesAllOnes_wavSize,
+                                         buffer);
+        const float sum {jdo::bufferSumElements(buffer)};
+        expectWithinAbsoluteError (sum, 128.0f, 0.001f);
+    }
+
+    beginTest ("nextPowerOf2()");
+
+    {
+        int x = 55;
+        expectEquals(jdo::nextPowerOf2(x), 64);
+        x = -234;
+        expectEquals(jdo::nextPowerOf2(x), 1);
+        x = 16300;
+        expectEquals(jdo::nextPowerOf2(x), 16384);
+    }
 }
