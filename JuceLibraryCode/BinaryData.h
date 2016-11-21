80b94dfd96187b5a2861c8c8bd97a87d364ed175
John Flynn <johnflynn@users.noreply.github.com>
2016-11-21 09:01:37 +0000

    Add JuceLibraryCode to repo (less reliant on Projucer).

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,26 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#ifndef BINARYDATA_H_115001851_INCLUDED
+#define BINARYDATA_H_115001851_INCLUDED
+
+namespace BinaryData
+{
+    extern const char*   Stereo64SamplesAllOnes_wav;
+    const int            Stereo64SamplesAllOnes_wavSize = 428;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Number of elements in the namedResourceList array.
+    const int namedResourceListSize = 1;
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
+}
+
+#endif
