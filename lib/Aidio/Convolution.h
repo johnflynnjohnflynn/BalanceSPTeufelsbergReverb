4950817cea02ef735735af87189b2bb0bdcc6f90
John Flynn <johnflynn@users.noreply.github.com>
2016-11-23 19:18:18 +0000

    Update libraries.

--- lib/Aidio/Convolution.h
+++ lib/Aidio/Convolution.h
@@ -27,11 +27,7 @@
 #ifndef CONVOLUTION_H_INCLUDED
 #define CONVOLUTION_H_INCLUDED
 
-#include <cassert>
-#include "gsl.h"
-#include "Utility.h"
 #include "Buffer.h"
-#include "Resampling.h"
 #include "WDL/convoengine.h"
 
 namespace ado
@@ -41,81 +37,49 @@ namespace ado
 /**
     Simple, zero latency convolution engine built on WDL.
     
+    @example    ado::Buffer ir {1, 1};      // member variables
+                ado::Convolution engine {ir};
+                                            
+                                            // put in constructor
+                jdo::bufferLoadFromWavBinaryData (BinaryData::IR_wav,
+                                                  BinaryData::IR_wavSize,
+                                                  ir, 
+                                                  44100);
+                engine.set (ir);
+                
+                engine.reset (sampleRate);  // put in prepareToPlay()
+                
+                                            // put in processBlock()
+                engine.process (buffer.getArrayOfWritePointers(),
+                                buffer.getNumChannels(),
+                                buffer.getNumSamples());
+    
     Notes: 
     - Max 4 channels!!!
     - If impulse has same data in all channels, WDL treats signal as mono and
-      won't work > 2 channels.
+      won't work > 2 channels. Watch for this!
 
 */
 class Convolution
 {
 public:
-    explicit Convolution (const ado::Buffer& impulse)
-        : lastSampleRate (impulse.getSampleRate()),
-          irOriginal {impulse}
-    {
-        set (irOriginal);
-    }
+    explicit Convolution (const ado::Buffer& impulse);
     ~Convolution() {}
 
-    void set (const ado::Buffer& impulse)
-    {
-        imp.Set (impulse.getReadArray(), impulse.getNumSamples(), impulse.getNumChannels());
-        eng.SetImpulse (&imp);
-    }
+    void set (const ado::Buffer& impulse);
 
-    void reset (double sampleRate)
-    {
-        eng.Reset();
+    void resampleIrOnRateChange (double sampleRate);
 
-        if (sampleRate != lastSampleRate)
-        {
-            irResampled = ado::resampleBuffer (irOriginal, sampleRate);
-
-            const double scale = irOriginal.getSampleRate() / sampleRate; // more samples convolved = louder!
-            irResampled *= scale;
-
-            set (irResampled);
-
-            lastSampleRate = sampleRate;
-        }
-    }
-
-    void process (ado::Buffer& block)
-    {
-        convolve (block.getWriteArray(), block.getNumChannels(), block.getNumSamples());
-    }
-
-    void process (float** block, int blockNumChannels, int blockNumSamples)
-    {
-        convolve (block, blockNumChannels, blockNumSamples);
-    }
+    void process (ado::Buffer& block);
+    void process (float** block, int blockNumChannels, int blockNumSamples);
 
 private:
-    void convolve (float** block, int blockNumChannels, int blockNumSamples)
-    {
-        eng.Add (block,                                 // Send input to conv eng
-                 blockNumSamples,
-                 blockNumChannels);
-
-        const int avail = eng.Avail (blockNumSamples);  // Confirm full buffer available
-        assert (avail == blockNumSamples);
-
-        float** convolved = eng.Get();
-        auto constConvolved = const_cast<const float**> (convolved);
-
-        ado::rawBufferCopy (constConvolved,             // source
-                            block,                      // dest
-                            blockNumChannels,
-                            blockNumSamples);
-
-        eng.Advance (blockNumSamples);                  // Advance the eng
-    }
-
+    void convolve (float** block, int blockNumChannels, int blockNumSamples);
 
     double lastSampleRate;
-    ado::Buffer irOriginal;                                             // lots of copies of the impulse now! Eek!
-    ado::Buffer irResampled {1, 1};
+
+    const ado::Buffer& irOriginal;
+          ado::Buffer  irResampled {1, 1};
 
     WDL_ImpulseBuffer imp;
     WDL_ConvolutionEngine_Div eng;
