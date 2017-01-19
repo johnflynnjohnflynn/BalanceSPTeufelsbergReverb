9adfafcf0facf92b7573fde08ea81992ec79fa4c
John Flynn <johnflynn@users.noreply.github.com>
2017-01-19 08:42:44 +0000

    Update libraries.

---
+++ Source/Judio/src/Button.cpp
@@ -0,0 +1,22 @@
+//--------//--------//--------//--------//--------//--------//--------//--------
+/*
+  ==============================================================================
+
+   This file follows the same open source licensing as the JUCE library.
+   Copyright (c) 2016 John Flynn
+
+   Permission is granted to use this software under the terms of either:
+   a) the GPL v2 (or any later version)
+   b) the Affero GPL v3
+
+   Details of these licenses can be found at: www.gnu.org/licenses
+
+   Judio is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+   A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+  ==============================================================================
+*/
+//--------//--------//--------//--------//--------//--------//--------//--------
+
+#include "Judio/Button.h"
